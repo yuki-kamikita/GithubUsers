753a61e19c2ba481dc2ff6dc289e9ddab561cb89
shuhei_kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-09-22 17:14:19 +0900

    ユーザーリストのデザイン調整

--- app/src/main/java/com/example/githubusers/MainActivity.kt
+++ app/src/main/java/com/example/githubusers/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.githubusers
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.githubusers.core.data.User
 import com.example.githubusers.model.remote.ApiRequest
 import com.example.githubusers.ui.theme.GithubUsersTheme
 import kotlinx.coroutines.CoroutineScope
@@ -31,7 +39,7 @@ class MainActivity : ComponentActivity() {
                             modifier = Modifier.fillMaxSize(),
                             color = MaterialTheme.colorScheme.background
                         ) {
-                            Greeting(userList.toString())
+                            UserCardList(userList)
                         }
                     }
                 }
@@ -41,14 +49,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun UserCardList(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->
+            UserCard(user)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun UserCard(user: User) {
+    Card(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth(),
+//        elevation = CardDefaults.cardElevation()
+    ) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(user.avatarUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+            Column(modifier = Modifier.padding(start = 8.dp)) {
+
+                Text(text = user.login)
+
+            }
+        }
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun ListPreview() {
+    val sampleUser = User(
+        login = "octocat",
+        id = 1,
+        nodeId = "MDQ6VXNlcjE=",
+        avatarUrl = "https://github.com/images/error/octocat_happy.gif",
+        gravatarId = "",
+        url = "https://api.github.com/users/octocat",
+        htmlUrl = "https://github.com/octocat",
+        followersUrl = "https://api.github.com/users/octocat/followers",
+        followingUrl = "https://api.github.com/users/octocat/following{/otherUser}",
+        gistsUrl = "https://api.github.com/users/octocat/gists{/gist_id}",
+        starredUrl = "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        subscriptionsUrl = "https://api.github.com/users/octocat/subscriptions",
+        organizationsUrl = "https://api.github.com/users/octocat/orgs",
+        reposUrl = "https://api.github.com/users/octocat/repos",
+        eventsUrl = "https://api.github.com/users/octocat/events{/privacy}",
+        receivedEventsUrl = "https://api.github.com/users/octocat/received_events",
+        type = "User",
+        siteAdmin = false,
+        bio = "ここに自己紹介がつらつらと書かれてそれを参考にユーザーの詳細を見に行くかどうか判断する想定"
+    )
     GithubUsersTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            UserCardList(
+                listOf(sampleUser, sampleUser, sampleUser)
+            )
+        }
     }
 }
