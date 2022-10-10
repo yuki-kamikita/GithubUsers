8507d65335d9a2fd4b127aeb307978bed6450006
shuhei_kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-10-10 20:53:16 +0900

    汎用コンポーネント作成
    中央に配置されるプログレスインジケーター

---
+++ app/src/main/java/com/example/githubusers/ui/component/CenterProgressIndicator.kt
@@ -0,0 +1,27 @@
+package com.example.githubusers.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/**
+ * 中央にいい感じに配置される丸型プログレスインジケーター
+ */
+@Composable
+fun CenterProgressIndicator() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CircularProgressIndicator()
+    }
+}
